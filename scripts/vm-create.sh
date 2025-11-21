9a6188c4ada95fd1c083241f38dce40abf0b24cf
Andrey <cshmuch@mail.ru>
2025-11-21 18:59:46 +0300

    pass json path to vm-create.sh

--- scripts/vm-create.sh
+++ scripts/vm-create.sh
@@ -10,4 +10,4 @@ yc compute instance create \
   --create-boot-disk name=itmo508541-disk,type=network-ssd,size=10,image-folder-id=standard-images,image-family=ubuntu-2404-lts \
   --network-interface subnet-name=itmo508541-subnet,nat-ip-version=ipv4 \
   --metadata-from-file user-data=./scripts/user-data.yaml \
-  --format json  > ./build/instance.json || yc compute instance get --name itmo508541_vm --format=json > ./build/instance.json
+  --format json  > $1 || yc compute instance get --name itmo508541_vm --format=json > $1
--- scripts/vm-deploy.sh
+++ scripts/vm-deploy.sh
@@ -4,16 +4,16 @@ while ! nc -z -v -w30 $1 22; do
   sleep 1
 done
 
-ssh runner@$1 -o StrictHostKeyChecking=no "curl -fsSL https://get.docker.com -o get-docker.sh ;
+ssh $1 -o StrictHostKeyChecking=no "curl -fsSL https://get.docker.com -o get-docker.sh ;
   sudo bash ./get-docker.sh ;
   sudo chown runner:runner /srv ;
   rm /srv/deploy.yml || true"
 
 mkdir --parent ./build
 docker compose -f docker-compose.deploy.yml config > ./build/deploy.yml
-scp -P 22 -r ./build/deploy.yml runner@$1:/srv/deploy.yml
+scp -P 22 -r ./build/deploy.yml $1:/srv/deploy.yml
 
-ssh runner@$1 -o StrictHostKeyChecking=no "cd /srv ;
+ssh $1 -o StrictHostKeyChecking=no "cd /srv ;
   docker compose stop || true ;
   rm docker-compose.yml || true ;
   mv deploy.yml docker-compose.yml ;
