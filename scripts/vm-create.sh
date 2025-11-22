24866bc00fc9bcf6000c83c1a606858a3f763967
Andrey <cshmuch@mail.ru>
2025-11-22 22:28:14 +0300

    start vm if stopped

--- scripts/vm-create.sh
+++ scripts/vm-create.sh
@@ -10,4 +10,4 @@ yc compute instance create \
   --create-boot-disk name=itmo508541-disk,type=network-ssd,size=10,image-folder-id=standard-images,image-family=ubuntu-2404-lts \
   --network-interface subnet-name=itmo508541-subnet,nat-ip-version=ipv4 \
   --metadata-from-file user-data=./scripts/user-data.yaml \
-  --format json  > $1 || yc compute instance get --name itmo508541_vm --format=json > $1
+  --format json  > $1 || yc compute instance start --name itmo508541_vm --format=json > $1 || yc compute instance get --name itmo508541_vm --format=json > $1
